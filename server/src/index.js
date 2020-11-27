5c00764f19fd9045536818a0ebac1a69fb30ab9f
FranciscoJavierRicoPerez <fjrp8@alu.ua.es>
2020-11-27 22:52:42 +0100

    Añadido cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
-
+var cors = require('cors');
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 var authApp = require('./routes/auth');
 app.use('/', authApp);
